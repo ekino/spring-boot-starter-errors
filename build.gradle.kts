417eddcb5fd9e0c5232c58809013b1189fc48c65
philippe.agra <philippe.agra@ekino.com>
2021-09-27 12:49:34 +0200

    feat: changing kotlin dependencies scope

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,10 +23,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.apache.commons:commons-lang3")
 
+  compileOnly("org.jetbrains.kotlin:kotlin-reflect")
+  compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   compileOnly("org.springframework.boot:spring-boot-starter-data-jpa")
   compileOnly("org.springframework.boot:spring-boot-starter-data-rest")
   compileOnly("org.springframework.security:spring-security-web")
